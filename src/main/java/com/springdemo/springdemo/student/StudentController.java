976487fde94e6a0726a8aa3c6064e7c4dc6ecf78
Dev Taha <tahayasseralshorafa@gmail.com>
2023-06-18 14:28:27 +0300

    implementation dao layer

--- src/main/java/com/springdemo/springdemo/student/StudentController.java
+++ src/main/java/com/springdemo/springdemo/student/StudentController.java
@@ -1,10 +1,9 @@
 package com.springdemo.springdemo.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/students")
@@ -13,14 +12,30 @@ public class StudentController {
     // constructor injection with studentController using @Autowired
     // without @Autowired is already injected suing initiation constructor
     private StudentService service; // field injection with studentController using @Autowired
-
     public StudentController(StudentService service){
         this.service = service;
     }
 
+    @PostMapping
+    public Student save(@RequestBody Student student){
+        return service.save(student);
+    }
+    @GetMapping("/{email}")
+    public Optional<Student> findByEmail(@PathVariable("email") String email){
+        return service.findByEmail(email);
+    }
     @GetMapping
     public List<Student> findAllStudents(){
         return service.findAllStudent();
+    }
+
+    @PutMapping
+    public Student updateStudent(@RequestBody Student student){
+        return service.update(student);
+    }
+    @DeleteMapping("/{email}")
+    public void delete(@PathVariable("email") String email){
+        service.delete(email);
     }
 
 }
